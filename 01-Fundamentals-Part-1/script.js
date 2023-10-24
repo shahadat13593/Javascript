c70e3b0e1d123507bef56d0f4a900d1215a3f9e7
shahadat13593 <shahadat15-13593@diu.edu.bd>
2023-10-24 21:54:22 +0600

    part1

---
+++ 01-Fundamentals-Part-1/script.js
@@ -0,0 +1,57 @@
+"use strict";
+
+// ! Variables and values
+// let js = "amazing";
+// console.log(40 + 8 + 23 - 10);
+
+// console.log("Shahadat");
+// console.log(23);
+
+// let firstName = "Raiyan";
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
+
+// let first = "shahadat";
+// let firstNamePerson = "raiyan";
+// let first_name_person;
+
+// let jonas_shahadat = "jn";
+// let newNum = 243;
+
+// let person = "shahadat";
+// let PI = 3.1416;
+
+// let myFirstJob = "Coder";
+// let myCurrentJob = "Teacher";
+
+// let job1 = "Programmer";
+// let job2 = "Teacher";
+
+// console.log(myFirstJob);
+
+// ! Data Types
+// ? 1. Number : Floating point numbers -> Used for decimals and integers
+
+// ? 2. String : Sequence of characters -> Used for text
+
+// ? 3. Boolean :Logical type that only be true and false -> Used for taking decision
+
+// ? 4. Undefined : Value taken by a variable that is not yet defined (Empty value)
+
+// ? 5. Null : also means Empty value
+
+// ? 6. Symbol(ES2015) : Value that is unique and cannot be changed
+
+// ? 7.BigInt : Larger integers than the Number type can hold
+
+console.log(true);
+let javaScriptIsFun = true;
+
+if (javaScriptIsFun) {
+  console.log("Hey! Let's learn javaScript!");
+}
+
+console.log(typeof 22);
+console.log(typeof javaScriptIsFun);
+console.log(typeof "shahadat");
